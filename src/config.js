a6444b451636e2ac6a669bb0024306776a846911
mzfais <mzfais@gmail.com>
2024-05-21 09:30:31 +0700

    add project

---
+++ src/config.js
@@ -0,0 +1,4 @@
+const baseUrl = "https://api.themoviedb.org/3";
+const apiToken = "USE YOUR OWN TOKEN OR KEY";
+const imageUrl = "https://image.tmdb.org/t/p/w500/";
+export { baseUrl, apiToken, imageUrl };
